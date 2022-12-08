eefebe459815654a680a5666994ccd156d4eaff0
Aasem Hany <assemsoma541@gmail.com>
2022-12-08 21:50:04 +0200

    prepair SpritKit template and add game assets

---
+++ Failing Boxes/GameScene.swift
@@ -0,0 +1,17 @@
+//
+//  GameScene.swift
+//  Failing Boxes
+//
+//  Created by Aasem Hany on 07/12/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+ 
+    }
+    
+}
